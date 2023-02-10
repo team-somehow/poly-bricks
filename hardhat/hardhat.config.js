8605516c0e0b6c06f9e966552de44dc09438245b
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-02-11 01:45:09 +0530

    feat: update hardhat config, connect with quicknode, dump artifacts in frontend

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + "/.env" });
+const { QUICKNODE_URL, MATIC_PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        mumbai: {
+            url:
+                "https://muddy-solitary-knowledge.matic-testnet.discover.quiknode.pro/" +
+                QUICKNODE_URL,
+            accounts: [`0x${MATIC_PRIVATE_KEY}`],
+        },
+    },
+    paths: {
+        artifacts: "../frontend/src/artifacts",
+    },
 };
