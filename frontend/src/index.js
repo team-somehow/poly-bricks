ab1b315b665cd68ffc83558267765b86f7704810
Killjoy <aryaajitnair@gmail.com>
2023-02-10 22:56:43 +0530

    fix: disable wallet visibility

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,12 @@ import { AuthProvider } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 
 const appID = "15228f3413342e43873a94d2ce54df3bb36b39f2";
-const provider = new AuthProvider(appID);
+const provider = new AuthProvider(appID,{
+	alwaysVisible:false
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-		<ProvideAuth provider={provider}>
+		<ProvideAuth provider={provider} >
 			<App />
 		</ProvideAuth>
 );
@@ -19,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+	
